5a658f452bf11ed2e1bda5044e9ed074e0dd5e02
Kamil Józefowski <k.m.jozefowski@gmail.com>
2021-04-02 21:59:27 +0200

    Update controller - add mapper & db service

--- src/main/java/com/kodilla/backend/controller/RoomController.java
+++ src/main/java/com/kodilla/backend/controller/RoomController.java
@@ -1,40 +1,47 @@
 package com.kodilla.backend.controller;
 
 import com.kodilla.backend.domain.Bed;
+import com.kodilla.backend.domain.Room;
 import com.kodilla.backend.domain.RoomDto;
+import com.kodilla.backend.mapper.RoomMapper;
+import com.kodilla.backend.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("room")
+@RequiredArgsConstructor
 public class RoomController {
 
+    private final DbService dbService;
+    private final RoomMapper roomMapper;
+
     @GetMapping()
-    public List<RoomDto> getRooms(){
-        return new ArrayList<>();
+    public List<RoomDto> getRooms() {
+        List<Room> rooms = dbService.getAllRoom();
+        return roomMapper.mapToRoomDtoList(rooms);
     }
 
     @GetMapping("/{roomId}")
-    public RoomDto getRoom (Long roomId) throws RoomNotFoundException{
-        return new RoomDto(1L, 1L, "Superior Double", new Bed(0,2),4,220D);
+    public RoomDto getRoom(Long roomId) throws RoomNotFoundException {
+        return new RoomDto(1L, 1L, "Superior Double", new Bed(0, 2), 4, 220D);
     }
 
     @DeleteMapping("/{roomId}")
-    public void deleteRoom (Long roomId){
+    public void deleteRoom(Long roomId) {
 
     }
 
     @PutMapping("")
-    public RoomDto updateRoom (Long roomId){
-        return new RoomDto(1L, 1L, "Superior Double", new Bed(1,1),3,220D);
+    public RoomDto updateRoom(Long roomId) {
+        return new RoomDto(1L, 1L, "Superior Double", new Bed(1, 1), 3, 220D);
     }
 
     @PostMapping(value = "")
-    public void createRoom(RoomDto roomDto){
+    public void createRoom(RoomDto roomDto) {
 
     }
 }
