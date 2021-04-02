dedcbc0df2eb0cdaef44a8c5cd2489655a0e672a
Kamil Józefowski <k.m.jozefowski@gmail.com>
2021-04-02 21:58:09 +0200

    Connect to DB

--- src/main/java/com/kodilla/backend/domain/Room.java
+++ src/main/java/com/kodilla/backend/domain/Room.java
@@ -3,21 +3,35 @@ package com.kodilla.backend.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "room")
 public class Room {
 
+    @Id
+    @GeneratedValue
     private Long id;
 
+    @Column(name = "room_number")
+    @UniqueElements
     private Long roomNumber;
 
+    @Column(name = "room_type")
     private String roomType;
 
     private Bed beds;
 
+    @Column(name = "max_sleeps")
     private Integer maxSleeps;
 
+    @Column(name = "price")
     private Double price;
 }
