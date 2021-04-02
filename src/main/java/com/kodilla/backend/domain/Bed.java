b8ceb1ff3a555b907f57b121436ad61bba83041c
Kamil Józefowski <k.m.jozefowski@gmail.com>
2021-04-02 20:11:29 +0200

    Add first class

---
+++ src/main/java/com/kodilla/backend/domain/Bed.java
@@ -0,0 +1,15 @@
+package com.kodilla.backend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Bed {
+
+    private Integer singleBed;
+    private Integer doubleBed;
+
+}
