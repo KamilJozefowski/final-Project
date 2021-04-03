156612cbdb0c980598e490b0ed6f5f6a4dfb842c
Kamil Józefowski <k.m.jozefowski@gmail.com>
2021-04-03 20:32:56 +0200

    Update - connect controller with db

--- src/main/java/com/kodilla/backend/controller/RoomController.java
+++ src/main/java/com/kodilla/backend/controller/RoomController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 
 @RestController
 @RequestMapping("room")
@@ -19,29 +21,30 @@ public class RoomController {
     private final DbService dbService;
     private final RoomMapper roomMapper;
 
-    @GetMapping()
+    @GetMapping("")
     public List<RoomDto> getRooms() {
         List<Room> rooms = dbService.getAllRoom();
         return roomMapper.mapToRoomDtoList(rooms);
     }
 
     @GetMapping("/{roomId}")
-    public RoomDto getRoom(Long roomId) throws RoomNotFoundException {
-        return new RoomDto(1L, 1L, "Superior Double", new Bed(0, 2), 4, 220D);
+    public RoomDto getRoom(@PathVariable Long roomId) throws RoomNotFoundException {
+        return roomMapper.mapToRoomDto(dbService.getRoom(roomId).orElseThrow(RoomNotFoundException::new));
     }
 
-    @DeleteMapping("/{roomId}")
-    public void deleteRoom(Long roomId) {
-
+    @PostMapping(value = "" , consumes = APPLICATION_JSON_VALUE)
+    public void createRoom(@RequestBody RoomDto roomDto) {
+        dbService.saveRoom(roomMapper.mapToRoom(roomDto));
     }
 
     @PutMapping("")
-    public RoomDto updateRoom(Long roomId) {
-        return new RoomDto(1L, 1L, "Superior Double", new Bed(1, 1), 3, 220D);
+    public RoomDto updateRoom(@RequestBody RoomDto roomDto) {
+        return roomMapper.mapToRoomDto(dbService.saveRoom(roomMapper.mapToRoom(roomDto)));
     }
 
-    @PostMapping(value = "")
-    public void createRoom(RoomDto roomDto) {
-
+    @DeleteMapping("/{roomId}")
+    public void deleteRoom(@PathVariable Long roomId) {
+        dbService.deleteRoom(roomId);
     }
+
 }
